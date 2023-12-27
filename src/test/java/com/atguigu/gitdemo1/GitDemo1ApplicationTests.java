8c25cac4e9865b8adf8ec7450e125e5729b956b3
root671 <154891215+root671@users.noreply.github.com>
2023-12-27 08:25:02 +0800

    第一次提交

---
+++ src/test/java/com/atguigu/gitdemo1/GitDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gitdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
